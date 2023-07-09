eb0efc5b484f35b0f3113d6e552bc38a3990b838
mustafacobanlar <mcobanlar34@gmail.com>
2023-07-09 14:50:28 +0300

    Image upload request method changed from post to put

--- src/main/java/com/aws/endpoint/controller/ImageController.java
+++ src/main/java/com/aws/endpoint/controller/ImageController.java
@@ -1,10 +1,12 @@
 package com.aws.endpoint.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -34,4 +36,27 @@ public class ImageController {
             return "Failed to upload image";
         }
     }
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<byte[]> downloadImage(@PathVariable String fileName) {
+        try {
+            // Define the path of the image file
+            String filePath = UPLOAD_DIR + "/" + fileName;
+
+            // Read the image file as a byte array
+            File file = new File(filePath);
+            FileInputStream fis = new FileInputStream(file);
+            byte[] imageBytes = StreamUtils.copyToByteArray(fis);
+            fis.close();
+
+            // Create a response with the image bytes and appropriate headers
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(imageBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Return an error response
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
